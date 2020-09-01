21c47134757a203020c9074518e2941cfc476d04
wmaiouiru <wmaiouiru@gmail.com>
2020-08-31 22:26:48 -0700

    save work

--- src/main/java/com/infiniteskills/data/Application.java
+++ src/main/java/com/infiniteskills/data/Application.java
@@ -1,16 +1,70 @@
 package com.infiniteskills.data;
 
+import com.infiniteskills.data.entities.Address;
+import com.infiniteskills.data.entities.Bank;
+import com.infiniteskills.data.entities.Credential;
+import com.infiniteskills.data.entities.TimeTest;
 import com.infiniteskills.data.entities.User;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class Application {
     public static void main(String[] args) {
+        // runTimeTest();
+        // runUser();
+        // runBank();
+        // runUserAddress();
+        // runCredentialUser();
+        runUserCredential();
+    }
+    private static void runTimeTest(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            session.getTransaction().begin();
+
+            TimeTest test = new TimeTest(new Date());
+            session.save(test);
+            session.getTransaction().commit();
+
+            session.refresh(test);
+
+            System.out.println(test.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            session.close();
+            HibernateUtil.getSessionFactory().close();
+        }
+    }
+    private static void runBank(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Bank bank = new Bank();
+        bank.setName("Federal Trust");
+        bank.setAddressLine1("33 Wall st");
+        bank.setAddressLine2("N/A");
+        bank.setCity("New York");
+        bank.setState("NY");
+        bank.setZipCode("27914");
+        bank.setCreatedBy("Kevin Bowersox");
+        bank.setCreatedDate(new Date());
+        bank.setLastUpdatedBy("Kevin Bowersox");
+        bank.setLastUpdatedDate(new Date());
+        bank.setInternational(false);
+        bank.getContacts().put("MANGER", "Joe");
+        bank.getContacts().put("TELLER", "Mary");
+        session.save(bank);
+        transaction.commit();
+    }
+    private static void runUser(){
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         User user = new User();
-        user.setBirthDate(new Date());
+        user.setBirthDate(getMyBirthday());
         user.setCreatedBy("kevin");
         user.setCreatedDate(new Date());
         user.setEmailAddress("kmb385@yahoo.com");
@@ -20,6 +74,109 @@ public class Application {
         user.setLastUpdatedDate(new Date());
         session.save(user);
         session.getTransaction().commit();
+        session.refresh(user);
+        System.out.println(user.getAge());
+        /*
+        session.beginTransaction();
+        User dbUser = (User) session.get(User.class, user.getUserId());
+        dbUser.setFirstName("ve");
+        dbUser.setLastName("ve last");
+        dbUser.setLastUpdatedDate(new Date());
+        session.getTransaction().commit();
         session.close();
+
+         */
+    }
+
+    private static void runUserAddress(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        User user = new User();
+        Address address = new Address();
+        Address address2 = new Address();
+        user.setAge(22);
+        user.setBirthDate(new Date());
+        user.setCreatedBy("Kevin");
+        user.setCreatedDate(new Date());
+        user.setFirstName("Kevin");
+        user.setLastName("Bowersox");
+        user.setEmailAddress("hello");
+        user.setLastUpdatedBy("kmb");
+        user.setLastUpdatedDate(new Date());
+        setAddressFields(address);
+        setAddressFields2(address2);
+        user.getAddress().add(address);
+        user.getAddress().add(address2);
+        session.save(user);
+        transaction.commit();
+    }
+
+    private static void runCredentialUser(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        User user = new User();
+        user.setAge(22);
+        user.setBirthDate(new Date());
+        user.setCreatedBy("Kevin");
+        user.setCreatedDate(new Date());
+        user.setFirstName("Kevin");
+        user.setLastName("Bowersox");
+        user.setEmailAddress("hello");
+        user.setLastUpdatedBy("kmb");
+        user.setLastUpdatedDate(new Date());
+        Credential credential = new Credential();
+        credential.setUsername("kmb");
+        credential.setPassword("123");
+        credential.setUser(user);
+        session.save(credential);
+        transaction.commit();
+    }
+
+    private static void runUserCredential(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        User user = new User();
+        user.setAge(22);
+        user.setBirthDate(new Date());
+        user.setCreatedBy("Kevin");
+        user.setCreatedDate(new Date());
+        user.setFirstName("Kevin");
+        user.setLastName("Bowersox");
+        user.setEmailAddress("hello");
+        user.setLastUpdatedBy("kmb");
+        user.setLastUpdatedDate(new Date());
+        Credential credential = new Credential();
+        credential.setUsername("kmb");
+        credential.setPassword("123");
+        credential.setUser(user);
+        user.setCredential(credential); // manging both side of the relationship
+        session.save(credential);
+        transaction.commit();
+
+        User dbUser = (User) session.get(User.class, credential.getUser().getUserId());
+        System.out.println(dbUser.getFirstName());
+        System.out.println(dbUser.getCredential().getCredentialId());
+    }
+
+    private static Date getMyBirthday(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 1984);
+        calendar.set(Calendar.MONTH, 6);
+        calendar.set(Calendar.DATE, 19);
+        return calendar.getTime();
+    }
+    private static void setAddressFields(Address address){
+        address.setAddressLine1("line1");
+        address.setAddressLine2("line2");
+        address.setCity("Philadelphia");
+        address.setState("PA");
+        address.setZipCode("12345");
+    }
+    private static void setAddressFields2(Address address){
+        address.setAddressLine1("line1");
+        address.setAddressLine2("line2");
+        address.setCity("Philadelphia");
+        address.setState("CO");
+        address.setZipCode("12345");
     }
 }
--- src/main/java/com/infiniteskills/data/entities/User.java
+++ src/main/java/com/infiniteskills/data/entities/User.java
@@ -1,26 +1,48 @@
 package com.infiniteskills.data.entities;
 
-import java.util.Date;
+import org.hibernate.annotations.Formula;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name="finances_user")
 public class User {
+
+    /*
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.TABLE, generator="user_table_generator")
+    @TableGenerator(name="user_table_generator",
+        table="ifinances_keys", pkColumnName = "PK_NAME", valueColumnName = "PK_VALUE")
+
+    */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="USER_ID")
     private Long userId;
 
-    @Column(name="FIRST_NAME")
+    @Column(name="FIRST_NAME", updatable = true)
     private String firstName;
 
-    @Column(name="LAST_NAME")
+    @Column(name="LAST_NAME", updatable = true)
     private String lastName;
 
     @Column(name="BIRTH_DATE")
@@ -29,7 +51,7 @@ public class User {
     @Column(name="EMAIL_ADDRESS")
     private String emailAddress;
 
-    @Column(name="LAST_UPDATED_DATE")
+    @Column(name="LAST_UPDATED_DATE", updatable = true)
     private Date lastUpdatedDate;
 
     @Column(name="LAST_UPDATED_BY")
@@ -40,6 +62,52 @@ public class User {
 
     @Column(name="CREATED_BY")
     private String createdBy;
+
+    @ElementCollection
+    @CollectionTable(name="user_address", joinColumns = @JoinColumn(name="USER_ID"))
+    @AttributeOverrides({
+            @AttributeOverride(name="addressLine1", column = @Column(name="USER_ADDRESS_LINE_1")),
+            @AttributeOverride(name="addressLine2", column = @Column(name="USER_ADDRESS_LINE_2"))
+    })
+    private List<Address> address = new ArrayList<Address>();
+    /*
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="addressLine1", column = @Column(name="USER_ADDRESS_LINE_1")),
+            @AttributeOverride(name="addressLine2", column = @Column(name="USER_ADDRESS_LINE_2"))
+    })
+    private Address address;
+    */
+
+    // Inverse asscoiation
+    @OneToOne(mappedBy = "user")
+    private Credential credential;
+
+
+    @Formula("lower(datediff(curdate(), birth_date)/365)")
+    private int age;
+
+    public User() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Transient
+    private boolean valid;
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
 
     public Long getUserId() {
         return userId;
@@ -111,5 +179,21 @@ public class User {
 
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
+    }
+
+    public List <Address> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<Address> address) {
+        this.address = address;
+    }
+
+    public Credential getCredential() {
+        return credential;
+    }
+
+    public void setCredential(Credential credential) {
+        this.credential = credential;
     }
 }
